f9af94276a35350956f80dda80e0ca3849671e05
AlejandroHerr <alejandrohnc88@gmail.com>
2017-03-10 16:23:02 +0100

    Add absolute values testing

--- modules/df.sh
+++ modules/df.sh
@@ -1,21 +1,32 @@
 function read_df {
   local fs='\'$1'$'
-  echo $(echo "$df_reading" | grep -E $fs | awk '{ print $5 }' | sed 's/\%//g')
+  echo $(echo "$df_reading" | grep -E $fs | awk '{ print $3" "$4" "$5 }' | sed 's/\%//g')
 }
 
 function df_update {
   df_reading=$(df)
   for probe in "${!df_probe[@]}"; do
-    update_rrd "$db_folder/df_${df_name[probe]}.rrd" $(read_df "${df_probe[probe]}")
+    read used fre perc < <(read_df "${df_probe[probe]}")
+    update_rrd "$db_folder/df_${df_rrd_name[probe]}.rrd" $perc
+    update_rrd "$db_folder/df_free_${df_rrd_name[probe]}.rrd" $fre
+    update_rrd "$db_folder/df_used_${df_rrd_name[probe]}.rrd" $used
   done
 }
 
 function df_init {
   for probe in "${!df_probe[@]}"; do
-    dsname="${df_name[probe]}"
+    dsname="${df_rrd_name[probe]}"
     filename="$db_folder/df_$dsname.rrd"
     if [ ! -f "$filename" ]; then
       create_rrd "$filename" "$dsname"
+    fi
+    filename="$db_folder/df_used_$dsname.rrd"
+    if [ ! -f "$filename" ]; then
+      create_rrd "$filename" "used_$dsname"
+    fi
+    filename="$db_folder/df_free_$dsname.rrd"
+    if [ ! -f "$filename" ]; then
+      create_rrd "$filename" "free_$dsname"
     fi
   done
 }
@@ -32,4 +43,8 @@ fi
 if [ "$df_names" ]; then
   names=$df_names
   IFS=',' read -r -a df_name <<< "$names"
+fi
+if [ "$df_rrd_names" ]; then
+  rrd_names=$df_rrd_names
+  IFS=',' read -r -a df_rrd_name <<< "$rrd_names"
 fi
